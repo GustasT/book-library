298b6e6658d02fbf625a34ec48a2b0b9983c5cd8
Gustas Tutinas <gustas696@gmail.com>
2023-04-17 03:14:18 +0200

    filters complete and  working

--- index.js
+++ index.js
@@ -29,6 +29,8 @@ bookForm.addEventListener("submit", () => {
   } else if (formPrice.value < 0) {
     helperMessage.innerHTML = "price can not be negative?!";
   } else {
+    let id = new Date().getTime();
+
     function Book() {
       this.author = formAuthor.value;
       this.bookName = formBookName.value;
@@ -36,6 +38,7 @@ bookForm.addEventListener("submit", () => {
       this.year = formYear.value;
       this.price = formPrice.value;
       this.bookImageUrl = formBookImageUrl.value;
+      this.id = id;
     }
 
     let book = new Book();
@@ -50,7 +53,11 @@ bookForm.addEventListener("submit", () => {
 
     loadAllAuthors();
 
+    loadAllCategories();
+
     helperMessage.innerHTML = "book added!";
+
+    bookForm.reset();
   }
 });
 
@@ -102,47 +109,6 @@ function loadAllBooks() {
 
 // knygu atvaizdavimas
 
-// knygu filtravimas
-
-// function filterBooks(author) {
-//   const filteredBooks = JSON.parse(localStorage.getItem("bookList")).filter(
-//     (book) => book.author == author
-//   );
-
-//   console.log(filteredBooks);
-
-//   function loadFilteredBooks() {
-//     bookList.innerHTML = "";
-
-//     for (let i = 0; i < filteredBooks.length; i++) {
-//       let li = document.createElement("li");
-//       li.innerHTML = `<li>
-//       <div class="book-dummy">
-//         <div class="picture-wrapper">
-//           <img
-//             src="https://www.adobe.com/express/create/cover/media_178ebed46ae02d6f3284c7886e9b28c5bb9046a02.jpeg?width=400&format=jpeg&optimize=medium"
-//             alt="."
-//           />
-//         </div>
-//         <div>
-//           <p>${filteredBooks[i].author}</p>
-//           <p>${filteredBooks[i].bookName}</p>
-//           <p>${filteredBooks[i].category}</p>
-//           <p>${filteredBooks[i].year}</p>
-//           <p>${filteredBooks[i].price}€</p>
-//         </div>
-//       </div>
-//     </li>`;
-
-//       bookList.appendChild(li);
-//     }
-//   }
-
-//   loadFilteredBooks();
-// }
-
-//knygu filtravimas
-
 // autoriu atvaizdavimas
 
 const authorList = document.getElementById("authorList");
@@ -166,8 +132,9 @@ function loadAllAuthors() {
     let li = document.createElement("li");
     li.innerHTML = `<label
     >${authorArray[i]}
-    <input type="checkbox" id="${authorArray[i]}" />
-  </label>`;
+    <input type="checkbox" id="${authorArray[i]}" onclick="filterByAuthor('${authorArray[i]}')" class="author-checkbox"/>
+  </label>
+  `;
 
     authorList.appendChild(li);
   }
@@ -198,7 +165,7 @@ function loadAllCategories() {
     let li = document.createElement("li");
     li.innerHTML = `<label
     >${categoryArray[i]}
-    <input type="checkbox" id="${categoryArray[i]}" />
+    <input type="checkbox" id="${categoryArray[i]}" onclick="filterByCategory('${categoryArray[i]}')" class="category-checkbox"/>
   </label>`;
 
     categoryList.appendChild(li);
@@ -206,3 +173,132 @@ function loadAllCategories() {
 }
 
 // kategoriju atvaizdavimas
+
+let booksByAuthor = [];
+
+// filtravimas autoriais
+
+function filterByAuthor(author) {
+  const authorFilter = document.getElementById(author);
+
+  const filteredBooks = JSON.parse(localStorage.getItem("bookList")).filter(
+    (book) => book.author == author
+  );
+
+  if (!authorFilter.classList.contains("active-filter")) {
+    booksByAuthor.push(...filteredBooks);
+
+    console.log(booksByAuthor);
+
+    authorFilter.classList.add("active-filter");
+  } else {
+    booksByAuthor = booksByAuthor.filter(function (book) {
+      return book.author != author;
+    });
+
+    console.log(booksByAuthor);
+
+    authorFilter.classList.remove("active-filter");
+  }
+}
+
+// filtravimas autoriais
+
+// filtracimas kategorijom
+
+let booksByCategory = [];
+
+function filterByCategory(category) {
+  const categoryFilter = document.getElementById(category);
+
+  const filteredBooks = JSON.parse(localStorage.getItem("bookList")).filter(
+    (book) => book.category == category
+  );
+
+  if (!categoryFilter.classList.contains("active-filter")) {
+    booksByCategory.push(...filteredBooks);
+
+    console.log(booksByCategory);
+
+    categoryFilter.classList.add("active-filter");
+  } else {
+    booksByCategory = booksByCategory.filter(function (book) {
+      return book.category != category;
+    });
+
+    console.log(booksByCategory);
+
+    categoryFilter.classList.remove("active-filter");
+  }
+}
+
+// filtravimas kategorijom
+
+// ----------------------------------
+
+let activeBookArray = [];
+
+function merge() {
+  let ids = new Set(booksByAuthor.map((d) => d.id));
+  activeBookArray = [
+    ...booksByAuthor,
+    ...booksByCategory.filter((d) => !ids.has(d.id)),
+  ];
+
+  console.log(activeBookArray);
+}
+
+function loadFilteredBooks() {
+  merge();
+
+  if (activeBookArray == false) {
+    loadAllBooks();
+  } else {
+    bookList.innerHTML = "";
+
+    for (let i = 0; i < activeBookArray.length; i++) {
+      let li = document.createElement("li");
+      li.innerHTML = `<li>
+    <div class="book-dummy">
+      <div class="picture-wrapper">
+        <img
+          src="https://www.adobe.com/express/create/cover/media_178ebed46ae02d6f3284c7886e9b28c5bb9046a02.jpeg?width=400&format=jpeg&optimize=medium"
+          alt="."
+        />
+      </div>
+      <div>
+        <p>${activeBookArray[i].author}</p>
+        <p>${activeBookArray[i].bookName}</p>
+        <p>${activeBookArray[i].category}</p>
+        <p>${activeBookArray[i].year}</p>
+        <p>${activeBookArray[i].price}€</p>
+      </div>
+    </div>
+  </li>`;
+
+      bookList.appendChild(li);
+    }
+  }
+}
+
+// --------------------
+
+function clearBooksByAuthor() {
+  const checkboxes = document.getElementsByClassName("author-checkbox");
+
+  for (let i = 0; i < checkboxes.length; i++) {
+    if (checkboxes[i].checked) {
+      checkboxes[i].click();
+    }
+  }
+}
+
+function clearBooksByCategory() {
+  const checkboxes = document.getElementsByClassName("category-checkbox");
+
+  for (let i = 0; i < checkboxes.length; i++) {
+    if (checkboxes[i].checked) {
+      checkboxes[i].click();
+    }
+  }
+}
